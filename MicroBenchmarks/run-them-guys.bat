267bf4dfc84180727e094ba9ff05870b133b5323
CJJ <ysharp.design@gmail.com>
2023-03-11 14:12:32 -0500

    Rearrange microbenchmarks

--- MicroBenchmarks/run-them-guys.bat
+++ MicroBenchmarks/run-them-guys.bat
@@ -1,21 +1,21 @@
 @echo off
 
-cscript //nologo Fib32-and-Fact20.js
-
 cscript //nologo For-Loop-vs-Lexical-Scope-Stress.js
+
+cscript //nologo Fib32-and-Fact20.js
 
 cscript //nologo Eratosthenes-Sieve.js
 
 rem cscript //nologo Bubble-Sort-16k-Integers.js
 
-Fib32-and-Fact20.py
-
 For-Loop-vs-Lexical-Scope-Stress.py
+
+Fib32-and-Fact20.py
 
 Eratosthenes-Sieve.py
 
 dotnet ..\LinearLisp\bin\Debug\net6.0\LinearLisp.dll
 
-powershell .\Fib20-and-Fact20.ps1
-
 powershell .\For-Loop-vs-Lexical-Scope-Stress.ps1
+
+powershell .\Fib20-and-Fact20.ps1

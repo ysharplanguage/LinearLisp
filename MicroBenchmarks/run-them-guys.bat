9d356adbd41f39550b0e303ac510ea6cc88ed960
CJJ <ysharp.design@gmail.com>
2023-03-11 21:16:40 -0500

    More focused microbenchmarks
    LinearLisp faster than MS JScript but slower than Python with intensive random array accesses.

--- MicroBenchmarks/run-them-guys.bat
+++ MicroBenchmarks/run-them-guys.bat
@@ -1,6 +1,6 @@
 @echo off
 
-cscript //nologo For-Loop-vs-Lexical-Scope-Stress.js
+rem cscript //nologo For-Loop-vs-Lexical-Scope-Stress.js
 
 cscript //nologo Fib32-and-Fact20.js
 
@@ -8,7 +8,7 @@ cscript //nologo Eratosthenes-Sieve.js
 
 rem cscript //nologo Bubble-Sort-16k-Integers.js
 
-For-Loop-vs-Lexical-Scope-Stress.py
+rem For-Loop-vs-Lexical-Scope-Stress.py
 
 Fib32-and-Fact20.py
 
@@ -16,6 +16,6 @@ Eratosthenes-Sieve.py
 
 dotnet ..\LinearLisp\bin\Debug\net6.0\LinearLisp.dll
 
-powershell .\For-Loop-vs-Lexical-Scope-Stress.ps1
+rem powershell .\For-Loop-vs-Lexical-Scope-Stress.ps1
 
-powershell .\Fib20-and-Fact20.ps1
+rem powershell .\Fib20-and-Fact20.ps1
